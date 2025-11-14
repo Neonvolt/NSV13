8f36c23fff87668e538d6d5f6abf5015c8769597
Neonvolt <83385197+Neonvolt@users.noreply.github.com>
2025-11-14 13:27:28 +0000

    make Dockerfile buildable, update precompile for tgs

---
+++ dependenciesDockerfile.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+#Project dependencies file
+#Final authority on what's required to fully build the project
+
+# byond version
+export BYOND_MAJOR=515
+export BYOND_MINOR=1647
+
+#rust version
+export RUST_VERSION=1.73
+
+#rust_g git tag
+export RUST_G_VERSION=3.2.0
+
+#node version
+export NODE_VERSION=18
+export NODE_VERSION_PRECISE=18.14.2
+
+# SpacemanDMM git tag
+export SPACEMAN_DMM_VERSION=suite-1.8
+
+#auxmos version
+export AUXMOS_VERSION=v2.5.2-b
+
+# Python version for mapmerge and other tools
+export PYTHON_VERSION=3.11.2
